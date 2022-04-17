ea8795c1729a8508ab14f0baee7257778caa2e89
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2022-04-17 16:50:31 +0800

    细节大优化，改进PWA体验

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var APP_PREFIX = '545在线'
-var VERSION = '20220414'
-var VERSION_AZUSA_PATCH_USE = '20220412'
+var VERSION = '20220417'
+var VERSION_AZUSA_PATCH_USE = '20220414'
 var AZUSA_PATCH_SKIP_LIST = [
     './img/bai.png',
     './img/close.svg',
