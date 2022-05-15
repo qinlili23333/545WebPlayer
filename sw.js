4ffecfc1df32730261022d5506da076362b3e8fb
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2022-05-15 20:35:29 +0800

    退出壁纸模式

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var APP_PREFIX = '545在线'
-var VERSION = '20220514v2'
-var VERSION_AZUSA_PATCH_USE = '20220506'
+var VERSION = '20220515'
+var VERSION_AZUSA_PATCH_USE = '20220514v2'
 var AZUSA_PATCH_SKIP_LIST = [
     './img/bai.png',
     './img/wanan.png',
